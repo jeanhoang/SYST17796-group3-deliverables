3ba79ef0324288a9d10ef31051449458302ae9fd
jeanhoang <hoantran@sheridancollege.ca>
2020-08-14 17:13:53 -0400

    Add more methods into cards & GroupOfCards

--- StarterCode/src/group3/GroupOfCards.java
+++ StarterCode/src/group3/GroupOfCards.java
@@ -7,6 +7,7 @@ package group3;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 /**
  * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
@@ -21,21 +22,34 @@ public class GroupOfCards {
     private ArrayList<Card> cards;
     private int size;//the size of the grouping
 
-    public GroupOfCards(int size) {
-        this.size = size;
-    }
-
-    /**
-     * A method that will get the group of cards as an ArrayList
-     *
-     * @return the group of cards.
-     */
-    public ArrayList<Card> getCards() {
-        return cards;
-    }
+    public void createFullDeck(){
+		//Generate Cards
+		//Loop Through Suits
+		for(Suit cardSuit : Suit.values()){
+			//Loop through Values
+			for(Rank cardRank : Rank.values()){
+				//Add new card to the mix
+				this.cards.add(new Card(cardSuit, cardRank));
+			}
+		}
+	}
 
     public void shuffle() {
-        Collections.shuffle(cards);
+        	ArrayList<Card> tmpDeck = new ArrayList<Card>();
+	//Randomly pick from the old deck and copy values to the new deck
+	Random random = new Random();
+	int randomCardIndex = 0;
+	int originalSize = this.cards.size();
+	for(int i = 0; i<originalSize;i++){
+		//gen random num according to int randomNum = rand.nextInt((max - min) + 1) + min;
+		randomCardIndex = random.nextInt((this.cards.size()-1 - 0) + 1) + 0;
+		//throw random card into new deck
+		tmpDeck.add(this.cards.get(randomCardIndex));
+		//remove picked from old deck
+		this.cards.remove(randomCardIndex);
+	}
+	//set this.deck to our newly shuffled deck
+	this.cards = tmpDeck;
     }
 
     /**
@@ -51,5 +65,93 @@ public class GroupOfCards {
     public void setSize(int size) {
         this.size = size;
     }
-
-}//end class
+    	//Remove a card from the deck
+	public void removeCard(int i){
+		this.cards.remove(i);
+	}
+	//Get card from deck
+	public Card getCard(int i){
+		return this.cards.get(i);
+	}
+	
+	//Add card to deck
+	public void addCard(Card addCard){
+		this.cards.add(addCard);
+	}
+	
+	//Draw a top card from deck
+	public void draw(GroupOfCards deck){
+		//Add card to this deck from whatever deck its coming from
+		this.cards.add(deck.getCard(0));
+		//Remove the card in the deck its coming from
+		deck.removeCard(0);
+	}
+	
+	//Use to print out deck
+	public String toString(){
+		String cardListOutput = "";
+		int i = 0;
+		for(Card aCard : this.cards){
+			cardListOutput += "\n" + aCard.toString();
+			i++;
+		}
+		return cardListOutput;
+	}
+	
+	public void moveAllToDeck(GroupOfCards deck){
+		int thisDeckSize = this.cards.size();
+		//put cards in moveTo deck
+		for(int i = 0; i < thisDeckSize; i++){
+			deck.addCard(this.getCard(i));
+		}
+		//empty out the deck
+		for(int i = 0; i < thisDeckSize; i++){
+			this.removeCard(0);
+		}
+	}
+	
+	public int deckSize(){
+		return this.cards.size();
+	}
+	
+	//Calculate the value of deck
+	public int cardsValue(){
+		int totalValue = 0;
+		int aces = 0;
+		//For every card in the deck
+		for(Card aCard : this.cards){
+			//Switch of possible values
+			switch(aCard.getRank()){
+			case TWO: totalValue += 2; break;
+			case THREE: totalValue += 3; break;
+			case FOUR: totalValue += 4; break;
+			case FIVE: totalValue += 5; break;
+			case SIX: totalValue += 6; break;
+			case SEVEN: totalValue += 7; break;
+			case EIGHT: totalValue += 8; break;
+			case NINE: totalValue += 9; break;
+			case JACK: totalValue += 10; break;
+			case QUEEN: totalValue += 10; break;
+			case KING: totalValue += 10; break;
+			case ACE: aces += 1; break;
+			}			
+		}
+		
+		//Determine the total current value with aces
+		//Aces worth 11 or 1 - if 11 would go over 21 make it worth 1
+		for(int i = 0; i < aces; i++){
+			//If they're already at over 10 getting an ace valued at 11 would put them up to 22, so make ace worth one
+			if (totalValue > 10){
+				totalValue += 1;
+			}
+			else{
+				totalValue += 11;
+			}
+		}
+		
+		//Return
+		return totalValue;
+	
+	}
+	
+}
